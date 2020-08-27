5c07e836d528ccedabf5535aac449d9560ea1362
CtrlCandV <48903486+CtrlCandV@users.noreply.github.com>
2020-08-27 17:47:46 +0800

    Update getvideoData.py
    增加对域名变更的支持，删除了无效的HTTP请求头项

--- taohua/getvideoData.py
+++ taohua/getvideoData.py
@@ -11,13 +11,13 @@ class getvideoData(object):
             self.__url=url
         else:
             self.__url=url+'/'
-        
+        self.host=self.__url.replace('http://','').replace('https://','').split('/')[0]
         self.__xpathStr={
             '二级分类':['//h1[@class="ts"]/a/text()'],
             '名称':['//*[@id="thread_subject"]/text()'],
             '详情ID':['//td[@class="t_f"]/@id'],
             '详情':['//*[@id="%s"]/text()'],
-            'img':['//*[@id="%s"]//img/@file','//div[@class="pattl"]//img/@zoomfile'],
+            'img':['//*[@id="%s"]//img/@file','//div[@class="pattl"]//img/@zoomfile','/html/body/div[7]/div[4]/div[2]/div[1]/table/tbody/tr[1]/td[2]/div[2]/div/div[1]/table/tbody/tr/td/img/@src'],
             '下载名':['//div[@class="pattl"]//a[@target="_blank"]/text()','//td[@id="%s"]//a[@target="_blank"]/text()'],
             '下载链接':['//div[@class="pattl"]//a[@target="_blank"]/@href','//td[@id="%s"]//a[@target="_blank"]/@href']
         }
@@ -128,7 +128,11 @@ class getvideoData(object):
 
                     #(id,一级分类,二级分类,名称,介绍,str(图片链接列表),str(下载字典),1)
                     end=(self.__unicodeChange(videoId),self.__unicodeChange(videoClass),self.__unicodeChange(class2Data),self.__unicodeChange(nameData),self.__unicodeChange(json.dumps(xiangqing)),self.__unicodeChange(json.dumps(img)),self.__unicodeChange(json.dumps(donload)),1)
-                    self.__sql.insertVideoData(end)
+                    try:
+                        self.__sql.insertVideoData(end)
+                    except Exception as err:
+                        if 'PRIMARY' not in str(err):
+                            raise err
                     self.__sql.setWaitScanUrlUsed(videoId)
                 except Exception as err:
                     print(err)
@@ -164,17 +168,16 @@ class getvideoData(object):
         获得页面数据
         '''
         header={
-            "Host":"thzd.cc",
+            "Host":self.host,
             "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
             "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             "Accept-Language":"zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-            "Referer":"http://thzd.cc/forum.php",
+            "Referer":"http://"+self.host+"/forum.php",
             "Connection":"keep-alive",
-            "Cookie":"WMwh_2132_saltkey=Pc84lF2l; WMwh_2132_lastvisit=1593879552; Hm_lvt_acfaccaaa388521ba7e29a5e15cf85ad=1593884505,1595118229; UM_distinctid=1731aeb75224d8-0918a33cff8d2f-4c302372-190140-1731aeb75233e7; CNZZDATA1254190848=850755625-1593883578-http%253A%252F%252Ft.thzdz3.com%252F%7C1595115184; HstCfa2810755=1593884514758; HstCla2810755=1595118260228; HstCmu2810755=1593884514758; HstPn2810755=2; HstPt2810755=6; HstCnv2810755=2; HstCns2810755=2; __dtsu=104015938845196792DC0326EFAD3B81; yunsuo_session_verify=305edb3a4f52e727ccda2155f10e6e29; WMwh_2132_lastact=1595116893%09home.php%09misc; Hm_lpvt_acfaccaaa388521ba7e29a5e15cf85ad=1595118260; WMwh_2132_st_t=0%7C1595116874%7C42ec8e6dd4f17591526dd100ab5e55a1; WMwh_2132_forum_lastvisit=D_181_1595116874; WMwh_2132_secqaa=58101.59f9204b91ba3c85dc; WMwh_2132_st_p=0%7C1595116892%7C6374ba4c29ba7de925d776f430e04670; WMwh_2132_viewid=tid_2210567",
             "Upgrade-Insecure-Requests":"1",
             "Cache-Control":"max-age=0"
         }
-        r=self.__session.get(url,headers=header,timeout=5,verify=False)
+        r=self.__session.get(url,headers=header,timeout=15,verify=False)
         return str(r.content,"utf-8")
     def __clearStr(self,data):
         '''
